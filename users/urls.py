d35a963f650a2fdd479d05abe95628ee5e168141
e-bsaid1 <ben199033@live.fr>
2022-12-20 10:52:07 +0100

    Ajout du module de la page d'inscription (URL pattern, fonction view, template)

--- users/urls.py
+++ users/urls.py
@@ -5,14 +5,20 @@
 ## 2) include pour ajouter les URL d'authentification par défaut que Django a défini
 from django.urls import path, include 
 
+#importe à partir de users les modules views pour écrire notre propre view pour la page d'inscription
+from . import views
+
+
 app_name = 'users' #variable permettant à Django de distinguer les URL patterns pour l'utilisateur des URL 
 				   #appartenant à d'autres applications (ex : learning_log)
 urlpatterns = [
 	# Include default auth urls. 
 	path('', include('django.contrib.auth.urls')),# colle à l'URL http://localhost:8000/users/login
-												  # users : demande à Django de regarder dans users/urls.py
-												  # login : appelle Django à envoyer des requêtes à la vue login par défaut du framework
-
+												  ## users : demande à Django de regarder dans users/urls.py
+												  ## login : appelle Django à envoyer des requêtes à la vue login par défaut du framework
+	# Registration page. 
+	path('register/', views.register, name='register'), # colle à l'URL http://localhost:8000/users/registers											  
+														# envoie des requêtes à la fonction register() que nous avons écrit 
 								
 ] 
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect #import° des fct render() et redirect()
+from django.contrib.auth import login #import° de la fonction login(), qui connecte l'utilisateur 
+from django.contrib.auth.forms import UserCreationForm #permet de créer un nouvel utilisateur pour utiliser l'appli web 
 
 # Create your views here.
+
+def register(request):
+	"""Register a new user."""
+	if request.method != 'POST':
+		#Display blank registration form.
+		form = UserCreationForm()
+	else:
+		# Process completed form. 
+		form = UserCreationForm(data=request.POST)
+
+		if form.is_valid(): 
+		# Si le pseudo a les bons caractères, avec les bons mdp, et sans mauvaises manips utilisateurs 
+			new_user = form.save()
+			# Connecte l'utilisateur et ensuite le redirige à la page d'acceuil
+			login(request, new_user)
+			return redirect('learning_logs:index')
+
+	# Affiche un formulaire vide ou invalide 
+	context = {'form':form}
+	return render(request, 'registration/register.html', context)
+
+
+
